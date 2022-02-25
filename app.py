6505caa6f0bc1819333ea0ccbd1bc6dfffc8bf61
shilpa reddy <shilpa.madhurig@gmail.com>
2022-02-25 13:02:39 +0100

    changed app.py without the date

--- app.py
+++ app.py
@@ -21,7 +21,13 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 - dropoff latitude
 - passenger count
 '''
-
+date_time = input('your date/time of travel')
+#user_time = input('your time')
+plat = input('your pick lat')
+plon = input('your pick lon')
+dlat = input('your drop lat')
+dlon = input('your drop lon')
+count = input('no. of passengers')
 '''
 ## Once we have these, let's call our API in order to retrieve a prediction
 
@@ -29,8 +35,9 @@ See ? No need to load a `model.joblib` file in this app, we do not even need to 
 
 🤔 How could we call our API ? Off course... The `requests` package 💡
 '''
-
+import requests
 url = 'https://taxifare.lewagon.ai/predict'
+
 
 if url == 'https://taxifare.lewagon.ai/predict':
 
@@ -46,3 +53,27 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
 ## Finally, we can display the prediction to the user
 '''
+from datetime import datetime
+import pytz
+
+# create a datetime object from the user provided datetime
+pickup_datetime = "2021-05-30 10:12:00"
+pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+
+# localize the user datetime with NYC timezone
+eastern = pytz.timezone("US/Eastern")
+localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+
+d = {
+  "pickup_datetime": formatted_pickup_datetime,
+  "pickup_longitude": plon,
+  "pickup_latitude": plat,
+  "dropoff_longitude": dlat,
+  "dropoff_latitude": dlon,
+  "passenger_count": count
+}
+params = d
+response = requests.get('url',params).json()
+st.write(response)
